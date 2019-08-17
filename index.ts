8787b63b670538e1a78cf85ee7822bf855f60392
Steven Dunham <dunhamsteve@gmail.com>
2019-08-17 14:31:36 -0700

    Add parser

--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
 type Var = string
 type Term = string | Var | ITerm
 interface ITerm extends Array<Term> {}
-
 type Rule = Term[]
-
 type List<T> = { car: T; cdr?: List<T> }
-
 type OList<T> = List<T> | undefined
 
 type Env = {
@@ -21,16 +18,12 @@ function lookup(key: string, e: Env) {
 const isvar = (x: any): x is Var => typeof x === "string" && x[0] === '?'
 
 function value(x: Term, e: Env): Term {
-  while (true) {
-    if (typeof x === 'string') {
-      let v = lookup(x,e)
-      if (v) {
-        x = v
-        continue
-      }
-    }
-    return x
+  while (typeof x === 'string') {
+    let v = lookup(x,e)
+    if (!v) break
+    x = v
   }
+  return x
 }
 
 function unify(x: Term, y: Term, e: Env): Env {
@@ -58,35 +51,70 @@ function copy(t: Term, n: number): Term {
   return t
 }
 
-function prove(goals: OList<Term>, rules: Rule[], env: Env, n:number) {
-  if (!goals) return printframe(env)
-  for (const rule of rules) { 
-    const a = instantiate(rule,n,goals.cdr)
-    const newenv = unify(goals.car, a.car, env)
-    if (newenv) { prove(a.cdr, db, newenv, n+1) }
+function run(goal: Term, db: Rule[]) {
+  function prove(goals: OList<Term>, rules: Rule[], env: Env, n:number) {
+    if (!goals) return console.log(pprint(['path','?a','?b','?c'],env))
+    for (const rule of rules) { 
+      const a = instantiate(rule,n,goals.cdr)
+      const newenv = unify(goals.car, a.car, env)
+      if (newenv) { 
+        prove(a.cdr, db, newenv, n+1) 
+      }
+    }
+    return
   }
+  function pprint(term: Term, env:Env): Term {
+    const tt = value(term,env) // chase vars
+    return tt instanceof Array ? `${tt[0]}(${tt.slice(1).map(x => pprint(x,env)).join(',')})` : tt
+  }
+  prove({car:goal}, db, {} as Env, 0)
 }
 
-function pprint(term: Term, env:Env): Term {
-  const tt = value(term,env) // chase vars
-  return tt instanceof Array ?
-    `${tt[0]}(${tt.slice(1).map(x => pprint(x,env)).join(',')})` : tt
+const ATOM = /^\??\w+$/
+
+function parse(str: string) {
+  const toks: string[] = []
+  str.replace(/\??\w+|[^\s]/g, (m) => (toks.push(m),''))
+  let p = 0
+  const error = (msg: string) => { throw `parse error: ${msg} at ${p} - ${JSON.stringify(toks.slice(p-1,p+2))}` }
+  const maybe = (tok: string) => toks[p] == tok && toks[p++]
+  const pAtom = (): Term => toks[p].match(ATOM) && toks[p++] || error('expected atom')
+  const pTerm = (): Term => {
+    let rval: Term = pAtom()
+    if (!maybe('(')) return rval
+    rval = [rval]
+    while (true) {
+      rval.push(pTerm())
+      if (maybe(')')) return rval
+      if (!maybe(',')) error('expected , or )')
+    }
+  }
+  const pRule = (): Rule => {
+    const rval: Rule = [pTerm()]
+    if (maybe(':')) {  
+      if (!maybe('-')) error('expected -')
+      while (true) {
+        rval.push(pTerm())
+        if (!maybe(',')) break
+      }
+    }
+    if (!maybe('.')) error('expected .')
+    return rval
+  }
+  const rules: Rule[] = []
+  while (p < toks.length) rules.push(pRule())
+  return rules
 }
 
-const printframe = (e:Env) => console.log(pprint(['path','?a','?b','?c'],e))
+const db = parse(`
+edge(a,b).
+edge(a,f).
+edge(a,g).
+edge(b,c).
+edge(b,d).
+path(?a,?b,cons(?a,cons(?b))) :- edge(?a,?b).
+path(?a,?c,cons(?a,?bc)) :- edge(?a,?b),path(?b,?c,?bc).
+`)
 
-const db: Rule[] = [
-  [['edge','a','b']],
-  [['edge','a','f']],
-  [['edge','a','g']],
-  [['edge','b','c']],
-  [['edge','b','d']],
-  // ...
-  [['path', '?A', '?B', ['cons','?A', ['cons','?B']]],
-    ['edge', '?A', '?B']],
-  [['path', '?A', '?C', ['cons', '?A', '?BC']],
-    ['edge', '?A', '?B'],
-    ['path', '?B', '?C', '?BC']],
-]
-
-prove({car:['path','?a','?b','?c']}, db, {} as Env, 0)
+const query = parse('path(?a,?b,?c).')[0][0]
+run(query,db)
